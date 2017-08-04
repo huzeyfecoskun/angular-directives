ff0d1ae6dd27d3b0bfde651c62f0400fecdb2469
huzeyfecoskun <huzeyfecoskun@hotmail.com>
2017-08-04 14:12:49 +0300

    Loading directive

---
+++ Loading/LoadingDirective.ts
@@ -0,0 +1,12 @@
+﻿
+var loading = angular.module("ui.loading", []);
+loading.directive("loading", () => {
+
+    return {
+
+        restrict: "A",
+        templateUrl:getaPath()+"/tr/api/template?key=loading"
+
+    };
+
+});
---
+++ Loading/readme.md
@@ -0,0 +1,6 @@
+# Loading Directive
+
+this directive requires loading.css and loading.html as component
+
+## Usage
+    <div loading />
