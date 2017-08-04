0b522e1fcb314d754c7b92dc0b3ece5036d1f6a9
huzeyfecoskun <huzeyfecoskun@hotmail.com>
2017-08-04 13:51:43 +0300

    last changes

--- DataTable/Datatable.ts
+++ DataTable/Datatable.ts
@@ -1,13 +1,93 @@
 ﻿var datatable = angular.module("ui.datatable", []);
 
+datatable.filter("dtfilter", () => {
+    return (item, a) => {
+
+        var o = [];
+
+        for (var i = 0; i < a.filter.length; i++) {
+
+            if (i === 0) {
+
+                if (a.original[i].type === "text") {
+
+                    if (a.filter[i] == undefined)
+                        a.filter[i] = "";
+
+                    var res = _.filter(item,
+                        (n) => {
+                            return n[a.original[i].id].indexOf(a.filter[i]) > -1;
+                        });
+
+                    for (var j = 0; j < res.length; j++)
+                        o.push(res[j]);
+
+                }
+                else if (a.original[i].type === "select") {
+
+                    if (a.filter[i] === undefined)
+                        a.filter[i] = {};
+
+                    var res = _.filter(item,
+                        (n) => {
+                            return n[a.original[i].id] === a.filter[i].value;
+                        });
+
+                    for (var j = 0; j < res.length; j++)
+                        o.push(res[j]);
+
+                }
+            } else {
+
+                if (a.original[i].type === "text") {
+
+                    if (a.filter[i] === undefined)
+                        a.filter[i] = "";
+
+                    o = _.filter(o,
+                        (n) => {
+                            return n[a.original[i].id].indexOf(a.filter[i]) > -1;
+                        });
+
+                } else {
+
+                    if (a.filter[i].value == undefined)
+                        a.filter[i].value = "";
+
+                    o = _.filter(item,
+                        (n) => {
+                            return n[a.original[i].id] === a.filter[i].value;
+                        });
+                }
+            }
+
+        }
+
+        o = _.uniqWith(o, _.isEqual);
+
+        return o;
+
+        //var pagination = [];
+
+        //for (var p = a.start; p < a.end; p++) {
+        //    if (p === o.length) {
+
+        //    }
+        //    if (o[p] != null)
+        //        pagination.push(o[p]);
+        //}
+
+        //return pagination;
+
+    };
+});
+
 datatable.filter("range",
     () => {
         return (item, a) => {
             var na = [];
-            for (var i = 0; i < item.length; i++) {
-                if (i >= a.start && i < a.end) {
-                    na.push(item[i]);
-                }
+            for (var i = a.start; i < a.end; i++) {
+                na.push(item[i]);
             }
             return na;
         };
@@ -24,30 +104,61 @@ datatable.directive("datatable",
             },
             templateUrl: getaPath() + "/tr/api/template?key=datatable",
             link: (scope, elem, attr) => {
+                scope.data = [];
+
+                try {
+                    scope.cfilter = new Array(scope.config.filter.length);
+                } catch (e) {
+
+                }
+
                 scope.currentpage = 0;
                 scope.pagesize = 50;
                 scope.pageCount = Math.floor(scope.data.length / scope.pagesize);
 
                 scope.$watch("data",
                     (nv, ov) => {
-                        if (scope.data.length % scope.pagesize === 0)
-                            scope.pageCount = Math.floor(scope.data.length / scope.pagesize);
-                        else
-                            scope.pageCount = Math.floor(scope.data.length / scope.pagesize) + 1;
+                        if (scope.config.enableFilter) {
+                            if (scope.result.length % scope.pagesize === 0)
+                                scope.pageCount = Math.floor(scope.result.length / scope.pagesize);
+                            else
+                                scope.pageCount = Math.floor(scope.result.length / scope.pagesize) + 1;
 
-                        if (scope.currentpage > scope.pageCount)
-                            scope.currentpage = 0;
+                            if (scope.currentpage > scope.pageCount)
+                                scope.currentpage = 0;
+                        } else {
+                            if (scope.data.length % scope.pagesize === 0)
+                                scope.pageCount = Math.floor(scope.data.length / scope.pagesize);
+                            else
+                                scope.pageCount = Math.floor(scope.data.length / scope.pagesize) + 1;
+
+                            if (scope.currentpage > scope.pageCount)
+                                scope.currentpage = 0;
+                        }
                     });
 
                 scope.$watch("pagesize",
                     (nv, ov) => {
-                        if (scope.data.length % scope.pagesize === 0)
-                            scope.pageCount = Math.floor(scope.data.length / scope.pagesize);
-                        else
-                            scope.pageCount = Math.floor(scope.data.length / scope.pagesize) + 1;
+                        if (scope.config.enableFilter) {
 
-                        if (scope.currentpage > scope.pageCount)
-                            scope.currentpage = 0;
+                            if (scope.result.length % scope.pagesize === 0)
+                                scope.pageCount = Math.floor(scope.result.length / scope.pagesize);
+                            else
+                                scope.pageCount = Math.floor(scope.result.length / scope.pagesize) + 1;
+
+                            if (scope.currentpage > scope.pageCount)
+                                scope.currentpage = 0;
+
+                            console.log("Datatable filter enabled", scope.result.length);
+                        } else {
+                            if (scope.data.length % scope.pagesize === 0)
+                                scope.pageCount = Math.floor(scope.data.length / scope.pagesize);
+                            else
+                                scope.pageCount = Math.floor(scope.data.length / scope.pagesize) + 1;
+
+                            if (scope.currentpage > scope.pageCount)
+                                scope.currentpage = 0;
+                        }
                     });
 
                 scope.sortType = "asc";
@@ -82,6 +193,8 @@ datatable.directive("datatable",
                     var m = new moment(i);
                     return m.format("DD.MM.YYYY");
                 };
+
+                scope.debug = () => { console.log(scope.cfilter); };
 
                 scope.pick = (item) => { scope.$emit("picked", item) };
                 scope.delete = (item) => { scope.$emit("delete", item) };
--- DataTable/readme.txt
+++ DataTable/readme.txt
@@ -10,10 +10,43 @@ Events
     
  Config
  
-     $scope.dtConfig =
-        {
-            columns: [
-                { id: "Artist", type: "text" },
-                { id: "SongName", type: "text" },
-                { id: "UploadDate", type: "date" }]
-        };
+     $scope.config = {
+                enableFilter : true,
+                filter:
+                [
+                    {
+                        id: "MemberType",
+                        type: "select",
+                        values: [{ var: "MemberType", key: "User", value: 0 },
+                        { var: "MemberType", key: "Admin", value: 1 },
+                        { var: "MemberType", key: "Super Admin", value: 2 }]
+                    },
+                    {
+                        id: "EMail",
+                        type: "text"
+                    }
+                ]
+                ,
+                columns: [
+                    {
+                        id: "MobilePhone",
+                        title: getTranslate("ta_mobile_phone", getLang()),
+                        type: "text",
+                        css: "width:100px;"
+                    }, {
+                        id: "Name",
+                        title: getTranslate("ta_name", getLang()),
+                        type: "text"
+                    },
+                    {
+                        id: "SureName",
+                        title: getTranslate("ta_sure_name", getLang()),
+                        type: "text"
+                    },
+                    {
+                        id: "EMail",
+                        title: getTranslate("ta_email", getLang()),
+                        type: "text"
+                    }
+                ]
+            };
